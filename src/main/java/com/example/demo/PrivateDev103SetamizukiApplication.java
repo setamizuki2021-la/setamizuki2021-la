1988f8304b8d5c95c0ab8012e2742b66f2136a94
student <student@WIN-NTTF67TPSJG>
2021-05-20 16:16:35 +0900

    初回

---
+++ src/main/java/com/example/demo/PrivateDev103SetamizukiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateDev103SetamizukiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateDev103SetamizukiApplication.class, args);
+	}
+
+}
