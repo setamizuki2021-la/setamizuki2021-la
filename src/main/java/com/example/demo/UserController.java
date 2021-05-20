e4576ae99d2f00df7143fccae76c87ee57bcd6f7
student <student@WIN-NTTF67TPSJG>
2021-05-20 18:30:20 +0900

    21.0520

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,5 +1,24 @@
 package com.example.demo;
 
-public class UserController {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+	@Autowired
+	HttpSession session;
+
+	/*
+	 * ログイン画面の表示
+	 */
+	// ログイン画面をlogin.htmlに表示する
+	@RequestMapping(value = "/")
+	public String login() {
+		// セッション画面を破棄する
+		session.invalidate();
+		return "login";
+	}
 }
